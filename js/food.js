fc2c886e66e71de0b1707abd1243b107297aa7e8
Alec Geatches <throwpi@me.com>
2013-05-12 13:01:53 -0600

    Added ExtraGrowFood type

--- js/food.js
+++ js/food.js
@@ -26,3 +26,37 @@ GrowFood.prototype = new Food();
 GrowFood.prototype.eaten = function(game) {
     game.snake.grow();
 }
+
+var ExtraGrowFood = function(max_x, max_y) {
+    var random_x = Math.floor(Math.random() * (max_x - 1));
+    var random_y = Math.floor(Math.random() * (max_y - 1));
+    this.location = {x: random_x, y: random_y};
+    this.color = "#08BB24";
+}
+ExtraGrowFood.prototype = new Food();
+ExtraGrowFood.prototype.draw = function(game) {
+    game.context.fillStyle = this.color;
+    for (var x = 0; x < 2; x++) {
+        for (var y = 0; y < 2; y++) {
+            game.context.fillRect((this.location.x + x) * game.grid_size, (this.location.y + y) * game.grid_size, game.grid_size, game.grid_size)
+        }
+    }
+}
+ExtraGrowFood.prototype.collided = function(snake) {
+    for (var i = 0; i < snake.body.length; i++) {
+        snake_part = snake.body[i];
+        for (var x = 0; x < 2; x++) {
+            for (var y = 0; y < 2; y++) {
+                if ((this.location.x + x) == snake_part.x && (this.location.y + y) == snake_part.y) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+ExtraGrowFood.prototype.eaten = function(game) {
+    game.snake.growth_amount *= 2;
+    game.snake.grow();
+    game.snake.growth_amount /= 2;
+}
