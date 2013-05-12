2d5f2aeaed4e4f5812b0e2611f3bb053ac978991
Alec Geatches <throwpi@me.com>
2013-05-12 13:28:47 -0600

    Added PortalFood type

--- js/food.js
+++ js/food.js
@@ -1,3 +1,4 @@
+// Food (abstract class) - Provides coordinates, color, and default draw and collision functions
 var Food = function(max_x, max_y) {
     var random_x = Math.floor(Math.random() * max_x);
     var random_y = Math.floor(Math.random() * max_y);
@@ -6,7 +7,7 @@ var Food = function(max_x, max_y) {
 }
 Food.prototype.draw = function(game) {
     game.context.fillStyle = this.color;
-    game.context.fillRect(this.location.x * game.grid_size, this.location.y * game.grid_size, game.grid_size, game.grid_size)
+    game.context.fillRect(this.location.x * game.grid_size, this.location.y * game.grid_size, game.grid_size, game.grid_size);
 }
 Food.prototype.collided = function(snake) {
     for (var i = 0; i < snake.body.length; i++) {
@@ -18,6 +19,7 @@ Food.prototype.collided = function(snake) {
     return false;
 }
 
+// GrowFood (basic food type)
 var GrowFood = function(max_x, max_y) {
     Food.call(this, max_x, max_y)
     this.color = "#12CF08";
@@ -27,11 +29,12 @@ GrowFood.prototype.eaten = function(game) {
     game.snake.grow();
 }
 
+// ExtraGrowFood (twice as wide and fat as normal food, causes 2x growth)
 var ExtraGrowFood = function(max_x, max_y) {
     var random_x = Math.floor(Math.random() * (max_x - 1));
     var random_y = Math.floor(Math.random() * (max_y - 1));
     this.location = {x: random_x, y: random_y};
-    this.color = "#08BB24";
+    this.color = "#08BB56";
 }
 ExtraGrowFood.prototype = new Food();
 ExtraGrowFood.prototype.draw = function(game) {
@@ -59,4 +62,37 @@ ExtraGrowFood.prototype.eaten = function(game) {
     game.snake.growth_amount *= 2;
     game.snake.grow();
     game.snake.growth_amount /= 2;
+}
+
+// PortalFood (creates two seperate, connected portals)
+var PortalFood = function(max_x, max_y) {
+    Food.call(this, max_x, max_y)
+    var random_x = Math.floor(Math.random() * max_x);
+    var random_y = Math.floor(Math.random() * max_y);
+    this.second_location = {x: random_x, y: random_y};
+    this.color = "#FF9409";
+    this.second_color = "#0C1FD4";
+}
+PortalFood.prototype = new Food();
+PortalFood.prototype.draw = function(game) {
+    game.context.fillStyle = this.color;
+    game.context.fillRect(this.location.x * game.grid_size, this.location.y * game.grid_size, game.grid_size, game.grid_size);
+    game.context.fillStyle = this.second_color;
+    game.context.fillRect(this.second_location.x * game.grid_size, this.second_location.y * game.grid_size, game.grid_size, game.grid_size);
+}
+PortalFood.prototype.collided = function(snake) {
+    var head = snake.body[0];
+    if (this.location.x == head.x && this.location.y == head.y) {
+        return true;
+    } else if (this.second_location.x == head.x && this.second_location.y == head.y) {
+        return true;
+    }
+    return false;
+}
+PortalFood.prototype.eaten = function(game) {
+    if (game.snake.body[0].x == this.location.x && game.snake.body[0].y == this.location.y) {
+        game.snake.body[0] = this.second_location;
+    } else {
+        game.snake.body[0] = this.location;
+    }
 }
